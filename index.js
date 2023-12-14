751faa182ee58089d2f5f280c5deb0aac53d0d85
Guilherme <guimaraes_gui@outlook.com.br>
2023-12-14 17:02:32 -0300

    Att

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 //Calculadora de partidas ranqueadas
-let wins = 15;
-let defeat = 3;
-let rank;
-let saldo_de_rankeada;
+let wins;
+let defeat;
 
-function saldo_ranked(wins, defeat){
-    saldo_de_rankeada = wins - defeat
-    return saldo_de_rankeada
+wins = 109
+defeat = 7
+function win_rate(wins, defeat){
+    return wins - defeat;
 }
 
 function elo(wins){
-    if (wins > 10){
+    if (wins < 10){
         rank = "Ferro"
-    } else if (wins > 11 && wins < 20){
-        rank = "Bronze"
-    } else if (wins > 21 && wins < 50){
-        rank = "Prata"
-    } else if (wins > 51 && wins < 80){
-        rank = "Ouro"
-    } else if (wins > 81 && wins < 90){
-        rank = "Diamante"
-    } else if (wins > 91 && wins < 100){
-        rank = "Lendário"
-    } else{
-        rank = "Imortal"
-    }
+        } else if (wins > 11 && wins < 20){
+            rank = "Bronze"
+        } else if (wins > 21 && wins < 50){
+            rank = "Prata"
+        } else if (wins > 51 && wins < 80){
+            rank = "Ouro"
+        } else if (wins > 81 && wins < 90){
+            rank = "Diamante"
+        } else if (wins > 91 && wins < 100){
+            rank = "Lendário"
+        } else{
+            rank = "Imortal"
+        }
 
     return rank
 }
 
-console.log("O Herói tem de saldo de " + saldo_ranked + " está no nível de " + elo);
+console.log("O Herói tem de saldo de " + win_rate(55, 3) + " está no nível de " + elo(wins));
